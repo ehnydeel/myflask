760d07510072fcd467c9c62d8eb298623a400988
andreas <andreas.kleinbub@gmail.com>
2018-09-20 15:12:49 +0200

    add comments to app.py

--- app.py
+++ app.py
@@ -1,18 +1,24 @@
 # app.py
 
+# import third-party modules
 from flask import Flask
 
+# import local modules
 from config import development as config
 from api import api
 
+# create flask-application
 app = Flask(__name__)
 
+# ROUTING - register blueprint-routes
 app.register_blueprint(api, url_prefix='/api')
 
+# ROUTING - setup entrypoint-route
 @app.route('/')
 def index():
     return 'Index page'
 
+# START FLASK-APPLICATION
 if __name__ == '__main__':
     app.run(host=config.HOST,
             port=config.PORT,
